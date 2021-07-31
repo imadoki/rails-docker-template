44c8f82ca725344e32c42a171d356e0bf141ec88
imadoki <imadoki-lemon@outlook.jp>
2021-08-01 00:50:08 +0900

    add redis_cache_store

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'mysql2', '~> 0.5'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
+gem 'hiredis', '~> 0.6.0'
 gem 'redis', '~> 4.0'
 gem 'redis-actionpack'
 # Use Active Model has_secure_password
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,5 +4,8 @@ class RootController < ApplicationController
   def index
     # test session
     session[:random_number] ||= Random.rand(100)
+    @raw_cache = Rails.cache.fetch('root_cache', expire_in: 12.hours) do
+      Random.rand(100)
+    end
   end
 end
